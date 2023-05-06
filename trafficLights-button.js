e40c497b1f5b637b5ab5818e636ccdb7836c646f
VladFocuta <132595962+VladFocuta@users.noreply.github.com>
2023-05-06 20:00:29 +0200

    Update trafficLights-button.js

--- trafficLights-button.js
+++ trafficLights-button.js
@@ -1,26 +1,10 @@
 let button = document.querySelector("button");
-let color = "red";
-setInterval(function () {
-    if (color == "red") {
-        button.style.backgroundColor = "yellow";
-        color = "yellow";
-    } else if (color == "yellow") {
-        button.style.backgroundColor = "green";
-        color = "green";
-    } else if (color == "green") {
-        button.style.backgroundColor = "red";
-        color = "red";
-    }
-}, 11000);
+let colors = ["red", "yellow", "green"];
+let currentColorIndex = 0;
 function changeColor() {
-    if (color == "red") {
-        button.style.backgroundColor = "yellow";
-        color = "yellow";
-    } else if (color == "yellow") {
-        button.style.backgroundColor = "green";
-        color = "green";
-    } else if (color == "green") {
-        button.style.backgroundColor = "red";
-        color = "red";
-    }
+  button.style.backgroundColor = colors[currentColorIndex];
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
 }
+setInterval(function () {
+    changeColor();
+}, 11000);
